00722bd9dfd1855a4509167d2187b5d3daed2292
Eduardo Gonçalves <milbratheduardo@gmail.com>
2024-05-13 17:00:36 -0300

    hero text

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
-import { headerLogo } from '../assets/images';
+import { levelOneLogo} from '../assets/images';
 import { hamburger } from '../assets/icons';
+import { navLinks } from '../constants';
 
 const Nav = () => {
   return (
@@ -7,15 +8,31 @@ const Nav = () => {
         <nav className='flex justify-between items-center max-container'>
             <a href="/">
                 <img 
-                 src={headerLogo}
+                 src={levelOneLogo}
                  alt='Logo'
                  width={130}
                  height={29}
                 />
             </a>
-            <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
-
+            <ul className='flex-1 flex justify-center items-center 
+            gap-16 max-lg:hidden'>
+                {navLinks.map((item) => (
+                    <li key={item.label}>
+                        <a href={item.href} className='font-montserrat leading-normal 
+                        text-lg text-slate-gray'>
+                            {item.label}
+                        </a>
+                    </li>
+                ))}
             </ul>
+            <div className='hidden max-lg:block'>
+                <img 
+                  src={hamburger}  
+                  alt='Hamburguer'
+                  width={25}
+                  height={25}
+                />
+            </div>
         </nav>
     </header>
   )
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,26 @@
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section
+    id="home"
+    className="w-full flex xl:flex-row flex-col 
+    justify-center min-h-screen gap-10 
+    max-container"
+    >
+    <div className="relative xl:w-2/5 flex flex-col justify-center 
+    items-start w-full max-xl:padding-x pt-28 ">
+      <p>Our Gaming Gear Highlights</p>
+      <h1>
+        <span>The New Arrivel</span>
+        <br />
+        <span>Level One</span>
+        Equipments
+      </h1>
+      <p>Dive into the new era of gaming with Level One's cutting-edge equipment,
+       enhancing every session with precision and style.</p>
+
+    </div>
+
+    </section>
   )
 }
 
